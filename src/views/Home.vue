8cc1e026572928e45d3db6b221f51ae8911991cc
Phil Davis <pdav6646@uni.sydney.edu.au>
2019-03-20 22:48:54 +1100

    adding vue-slider component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <vue-slider v-model="value"></vue-slider>
+    <HelloWorld msg="Welcome to Your Vue.js App"></HelloWorld>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/antd.css";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    VueSlider
+  },
+  data() {
+    return {
+      value: 50
+    };
   }
 };
 </script>
