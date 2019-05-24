7946ada0d0f97c7bcf4df4f8386d0c933d87a083
Phil Davis <pdav6646@uni.sydney.edu.au>
2019-05-24 16:46:18 +1000

    added BLE click

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,65 @@
       <router-link to="/about">About Phil</router-link>
     </div>
     <router-view />
+    <p>hello</p>
+    <button @click="onClick()">Click</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onClick() {
+      var serviceUuid = 0xffe0;
+      var characteristicUuid = 0xffe1;
+      console.log("Requesting Bluetooth Device...");
+
+      navigator.bluetooth
+        .requestDevice({ filters: [{ services: [serviceUuid] }] })
+        .then(device => {
+          console.log("Connecting to GATT Server...");
+          return device.gatt.connect();
+          // device.gatt.connect();
+          //console.log("connected");
+        })
+        .then(server => {
+          console.log("connected");
+          console.log("Getting Service...");
+          return server.getPrimaryService(serviceUuid);
+        })
+        .then(service => {
+          console.log("Getting Characteristic...");
+          return service.getCharacteristic(characteristicUuid);
+        })
+        .then(characteristic => {
+          let myCharacteristic = characteristic;
+          return myCharacteristic.startNotifications().then(_ => {
+            console.log("> Notifications started");
+            myCharacteristic.addEventListener(
+              "characteristicvaluechanged",
+              function handleNotifications(event) {
+                let value = event.target.value;
+                let a = [];
+                // Convert raw data bytes to hex values just for the sake of showing something.
+                // In the "real" world, you'd use data.getUint8, data.getUint16 or even
+                // TextDecoder to process raw data bytes.
+                for (let i = 0; i < value.byteLength; i++) {
+                  a.push(
+                    "0x" + ("00" + value.getUint8(i).toString(16)).slice(-2)
+                  );
+                }
+                console.log("> " + a.join(" "));
+              }
+            );
+          });
+        })
+        .catch(error => {
+          console.log("Argh! " + error);
+        });
+    }
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
